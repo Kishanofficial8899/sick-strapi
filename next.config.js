d23cf4ea94973e8115995b1776ab1fbc62c1cbe7
kishanpatel <kishanpatel3545@gmail.com>
2021-08-26 16:32:29 +0530

    Commitchanges

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     env: {
         PRODUCTION_URL: "https://strapidemobytes.herokuapp.com/graphql",
         DEVELOPMENT_URL: "https://strapidemobytes.herokuapp.com/graphql",
-        ENVIROMENT: "production"
+        ENVIROMENT: "production",
     },
 }
